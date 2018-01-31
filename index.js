61bfc9a8041fee01d3c1cd71486160ae796833ae
Avi Romanoff <avi@romanoff.me>
2018-01-30 19:16:38 -0500

    lots of compression of everything

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const controller = new ScrollMagic.Controller();
 
 function vw(v) {
-  const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  return (v * w) / 100;
+  const w = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0,
+  );
+  return v * w / 100;
 }
 
 // Slide hands in when scrolling
 const handsTimeline = new TimelineMax();
-const leftHandTween = TweenMax.to('.hand-left', 1, {left: 0});
-const rightHandTween = TweenMax.to('.hand-right', 1, {right: 0});
-handsTimeline.add([leftHandTween,rightHandTween]);
+const leftHandTween = TweenMax.to('.hand-left', 1, { left: 0 });
+const rightHandTween = TweenMax.to('.hand-right', 1, { right: 0 });
+handsTimeline.add([leftHandTween, rightHandTween]);
 const handsScene = new ScrollMagic.Scene({
   triggerHook: 0,
   duration: 600,
@@ -17,8 +20,8 @@ const handsScene = new ScrollMagic.Scene({
   reverse: true,
   triggerElement: '.table',
 })
-.setTween(handsTimeline)
-.addTo(controller);
+  .setTween(handsTimeline)
+  .addTo(controller);
 
 // Scale the logo down when sticky
 const logoTweenScene = new ScrollMagic.Scene({
@@ -28,16 +31,18 @@ const logoTweenScene = new ScrollMagic.Scene({
   reverse: true,
   triggerElement: '.table',
 })
-.setTween('.logo-container', 1, {scale: 0.6})
-.setTween('.l2', 1, {opacity: 0})
-.addTo(controller);
+  .setTween('.logo-container', 1, { scale: 0.6 })
+  .setTween('.l2', 1, { opacity: 0 })
+  .addTo(controller);
 
-// Flatten batman out for mobile devices
+// Expand batman to full width
+// This is necessary for mobile, as the content
+// width is wider than the batman element.
 const lexLuther = new ScrollMagic.Scene({
   triggerHook: 0,
   offset: vw(-8),
   reverse: true,
   triggerElement: 'h1',
 })
-.setClassToggle('.batman', 'batman-fullwidth')
-.addTo(controller);
+  .setClassToggle('.batman', 'batman-fullwidth')
+  .addTo(controller);
